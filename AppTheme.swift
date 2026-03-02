627c6933c2a0544e7780aabdad3a9f273ca7fce3
Daiwiik Harihar <145755568+daiv09@users.noreply.github.com>
2026-03-02 10:54:54 +0530

    final app that was submitted

---
+++ AppTheme.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  HowToTrainAModel-2
+//
+//  Created by Daiwiik on 27/02/26.
+//
+
+import Foundation
---
+++ Screens/HowToStep.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  HowToTrainAModel-2
+//
+//  Created by Daiwiik on 27/02/26.
+//
+
+import SwiftUI
+
+struct HowToStep: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HowToStep()
+}
